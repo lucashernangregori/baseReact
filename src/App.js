5e4a184a807986f8e25e8ff5961f1119ab55ee6d
lgregori <lgregori@gmail.com>
2020-10-31 03:47:44 -0300

    terminando primer capitulo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,96 @@
+import React, { Component } from "react";
 //import logo from './logo.svg';
 //import './App.css';
-
-
-function App() {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "Adam"
-    }
+      userName: "Lucas",
+      todoItems: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Jose", done: false },
+      ],
+      newItemText: "",
+    };
   }
 
-  return (
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  };
+
+  createNewTodo = () => {
+    if (
+      !this.state.todoItems.find(
+        (item) => item.action === this.state.newItemText
+      )
+    ) {
+      this.setState({
+        todoItems: [
+          ...this.state.todoItems,
+          { action: this.state.newItemText, done: false },
+        ],
+        newItemText: "",
+      });
+    }
+  };
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Lucas" ? "Bob" : "Lucas",
+    });
+  };
+
+  toggleTodo = (todo) => {
+    this.setState({
+      todoItems: this.state.todoItems.map((item) =>
+        item.action === todo.action ? { ...item, done: !item.done } : item
+      ),
+    });
+  };
+
+  todoTableRows = () =>
+    this.state.todoItems.map((item) => (
+      <tr key={item.action}>
+        <td>{item.action}</td>
+        <td>
+          <input
+            type="checkbox"
+            checked={item.done}
+            onChange={() => this.toggleTodo(item)}
+          />
+        </td>
+      </tr>
+    ));
+
+  render = () => (
     <div>
       <h4 className="bg-primary text-white text-center p-2">
-        To Do List
+        {this.state.userName} To Do List (
+        {this.state.todoItems.filter((t) => !t.done).length} items to do)
       </h4>
+      <div className="container-fluid">
+        <div className="my-1">
+          <input
+            className="form-control"
+            value={this.state.newItemText}
+            onChange={this.updateNewTextValue}
+          />
+          <button className="btn btn-primary mt-1" onClick={this.createNewTodo}>
+            Add
+          </button>
+        </div>
+        <table className="table table-stripped table-bordered">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>{this.todoTableRows()}</tbody>
+        </table>
+      </div>
     </div>
   );
 }
-
-export default App;
